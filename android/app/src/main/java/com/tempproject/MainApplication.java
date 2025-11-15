d80e82d3948fca7d1af1f4c3739d8e304d827ab3
cmarinbianchi-cloud <cmarinbianchi@gmail.com>
2025-11-15 00:52:53 -0300

    15 nov 2

--- android/app/src/main/java/com/tempproject/MainApplication.java
+++ android/app/src/main/java/com/tempproject/MainApplication.java
@@ -1,18 +1,33 @@
 package com.tempproject;
 
 import android.app.Application;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
-import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-    private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-    };
+    private final ReactNativeHost mReactNativeHost =
+        new ReactNativeHost(this) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+                List<ReactPackage> packages = new PackageList(this).getPackages();
+                return packages;
+            }
+
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
 
     @Override
     public ReactNativeHost getReactNativeHost() {
@@ -25,5 +40,6 @@ public class MainApplication extends Application implements ReactApplication {
         SoLoader.init(this, /* native exopackage */ false);
     }
 }
+
 
 
